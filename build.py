af0764fb08515120d8c7330c81f86c395f3339db
Abhilash <abhilashbabuj@gmail.com>
2022-04-17 15:23:51 +0200

    Use only build.py instead of Makefile

--- build.py
+++ build.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import re
+from shutil import copyfile
+import os
 
 summary = Path.cwd() / "BookSummary" / "Summary.md"
 
@@ -15,6 +17,16 @@ with open(summary) as f:
         else:
             lines.append(line)
 
-with open(Path.cwd() / "BookSummary" / "index.md", "w") as f:
+with open(Path.cwd() / "public" / "index.md", "w") as f:
     for line in lines:
         f.write(line)
+
+book_summary_path = Path.cwd() / "BookSummary"
+
+for md_file in book_summary_path.rglob("*.md"):
+    old_fname = md_file.name
+    new_fname = old_fname.replace(" ", "_")
+    new_fname = new_fname.lower()
+    dest_path = Path.cwd() / "public" / new_fname
+    copyfile(str(md_file), str(dest_path))
+    os.system(f"pandoc -o {dest_path.with_suffix('.html')} {dest_path} -c style1.css")
